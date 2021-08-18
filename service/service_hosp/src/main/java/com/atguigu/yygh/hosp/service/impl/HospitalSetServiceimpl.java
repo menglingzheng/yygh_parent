ceb93bf67a38deb124e3629c0e98db40d1eff53f
mlzheng <2455156355@qq.com>
2021-08-18 20:36:24 +0800

    Initial commit

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/HospitalSetServiceimpl.java
@@ -0,0 +1,21 @@
+package com.atguigu.yygh.hosp.service.impl;
+
+import com.atguigu.yygh.hosp.mapper.HospitalSetMapper;
+import com.atguigu.yygh.hosp.service.HospitalSetService;
+import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.vo.order.SignInfoVo;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HospitalSetServiceimpl extends ServiceImpl<HospitalSetMapper, HospitalSet> implements HospitalSetService {
+    @Override
+    public String getSignKey(String hoscode) {
+        return null;
+    }
+
+    @Override
+    public SignInfoVo getSignInfoVo(String hoscode) {
+        return null;
+    }
+}
